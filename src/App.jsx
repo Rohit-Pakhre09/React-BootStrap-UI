c56bf191fccc92a6a3a7cd1c297e51224ced3b64
Rohit-Pakhre <pakhrerohit281@gmail.com>
2025-07-22 18:36:09 +0530

    Feat: Layout set.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import Header from "./components/Header"
-import MainPage from "./components/MainPage"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Information from "./components/Information";
+import Image from "./components/Image";
 
 const App = () => {
   return (
-    <section className="vh-100">
-      <Header />
-      <MainPage />
-    </section>
-  )
-}
+    <section className="min-vh-100">
+      <div className="container h-100">
+        <div className="row h-100 d-flex justify-content-center align-items-center">
+          <div className="col-12 col-md-5 col-md-7 col-lg-7 d-flex">
+            <Information />
+          </div>
 
-export default App
+          <div className="col-12 col-md-7 col-lg-5 d-flex justify-content-end">
+            <Image />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default App;
